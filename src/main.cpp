fbad5f5418f9aad0fab39d0200fec73e83567536
chuategiaitich <nguyenminhtue148@gmail.com>
2026-01-28 23:34:50 +0700

    add reading adc ThreshHold to prevent reload screen manually

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,33 @@ U8X8_SSD1306_128X64_NONAME_HW_I2C oled(U8X8_PIN_NONE);
 #define BTN3 PIN_PD6
 #define BTN4 PIN_PD7
 #define POTENTIONMETER PIN_PC0
+// #define ADC_SAMPLES 8
+#define ADC_THRESHOLD 100
 
-uint16_t readADC() {
-  ADCSRA |= (1 << ADSC);
-  while (ADCSRA & (1 << ADSC));
-  return ADC;
-}
+// uint16_t adcBuffer[ADC_SAMPLES];
+// uint8_t adcIndex = 0;
+// uint32_t adcSum = 0;
+uint16_t lastAdcValue = 0xFFFF;
+uint8_t lastBtnState = 0xFF;
 
-void setup() {
+// uint16_t readAdcFiltered() {
+//   uint16_t raw = analogRead(POTENTIONMETER);
+
+//   adcSum -= adcBuffer[adcIndex];
+//   adcBuffer[adcIndex] = raw;
+//   adcSum += raw;
+
+//   adcIndex++;
+//   if (adcIndex >= ADC_SAMPLES) adcIndex = 0;
+
+//   return adcSum / ADC_SAMPLES;
+// }
+
+void setup()
+{
   oled.begin();
   oled.setPowerSave(0);
-  oled.setFont(u8x8_font_chroma48medium8_r);
+  oled.setFont(u8x8_font_8x13_1x2_r);
   pinMode(POTENTIONMETER, INPUT);
   pinMode(BTN1, INPUT);
   pinMode(BTN2, INPUT);
@@ -25,16 +41,38 @@ void setup() {
   pinMode(BTN4, INPUT);
 }
 
-void loop() {
-  char buf[8];
+void loop()
+{
   uint16_t adcValue = analogRead(POTENTIONMETER);
-  itoa(adcValue, buf, 10);
-  oled.clearDisplay();
-  oled.drawString(0, 0, "ADC0:");
-  oled.drawString(6, 0, buf);
-  if(digitalRead(BTN1)==1)oled.drawString(0, 5, "PD4");
-  if(digitalRead(BTN2)==1)oled.drawString(0, 5, "PD5");
-  if(digitalRead(BTN3)==1)oled.drawString(0, 5, "PD6");
-  if(digitalRead(BTN4)==1)oled.drawString(0, 5, "PD7");
-  delay(100);
+
+  uint8_t btnState = 0;
+  btnState |= digitalRead(BTN1) << 0;
+  btnState |= digitalRead(BTN2) << 1;
+  btnState |= digitalRead(BTN3) << 2;
+  btnState |= digitalRead(BTN4) << 3;
+
+  // if (adcValue != lastAdcValue || btnState != lastBtnState)
+  if (abs(adcValue - lastAdcValue) > ADC_THRESHOLD || btnState != lastBtnState) 
+  {
+    lastAdcValue = adcValue;
+    lastBtnState = btnState;
+
+    char buf[8];
+    itoa(adcValue, buf, 10);
+
+    oled.clearDisplay();
+    oled.drawString(0, 0, "ADC0:");
+    oled.drawString(6, 0, buf);
+
+    uint8_t y = 4;
+
+    if (btnState & (1 << 0))
+      oled.drawString(0, y++, "PD4");
+    if (btnState & (1 << 1))
+      oled.drawString(0, y++, "PD5");
+    if (btnState & (1 << 2))
+      oled.drawString(0, y++, "PD6");
+    if (btnState & (1 << 3))
+      oled.drawString(0, y++, "PD7");
+  }
 }
