2b1eea6c9311d3672423eeaeb55c4cf7c0245f70
chuategiaitich <nguyenminhtue148@gmail.com>
2026-01-30 11:14:12 +0700

    change to bare metal code

---
+++ REGISTER.md
@@ -0,0 +1,7 @@
+# THANH GHI SỬ DỤNG TRONG ATMEGA8L
+
+| Thanh ghi I/O | Thanh ghi ADC | Thanh ghi Timer1 |
+|---------------|---------------|------------------|
+| DDRx | ADMUX | TCCR1A |
+| PORTx | ADCSRA | TCCR1B |
+| PINx | | OCR1A |
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,22 @@
 #include <U8x8lib.h>
 U8X8_SSD1306_128X64_NONAME_HW_I2C oled(U8X8_PIN_NONE);
 
-#define SW2 PIN_PD4
-#define SW3 PIN_PD5
-#define SW4 PIN_PD6
-#define SW5 PIN_PD7
+#define SW2_PIN 4            // PD4
+#define SW3_PIN 5            // PD5
+#define SW4_PIN 6            // PD6
+#define SW5_PIN 7            // PD7
+#define POTENTIONMETER_PIN 0 // PC0
 
-#define POTENTIONMETER PIN_PC0
-
-#define L298_ENA PIN_PB1 // OC1A - PWM - TIM1A
-#define L298_IN1 PIN_PC2
-#define L298_IN2 PIN_PC3
+#define ENA_PIN 1 // PB1 (OC1A)
+#define IN1_PIN 2 // PC2
+#define IN2_PIN 3 // PC3
 
 #define DEBOUNCE_TIME 40
+
+#define SW2_READ() ((PIND & (1 << SW2_PIN)))
+#define SW3_READ() ((PIND & (1 << SW3_PIN)))
+#define SW4_READ() ((PIND & (1 << SW4_PIN)))
+#define SW5_READ() ((PIND & (1 << SW5_PIN)))
 
 uint8_t lastAdcValue = 0;
 uint8_t lastBtnState = 0;
@@ -23,13 +27,13 @@ bool motorEnable = false;
 bool motorDirection = true; // true: forward, false: reverse
 uint8_t pwmValue = 0;
 
-uint8_t prevMode = 0xFF;
+uint8_t prevMode = 0xff;
 bool prevMotorEnable = 2;
 bool prevMotorDirection = 2;
 
-bool buttonPressed(uint8_t pin, uint8_t index)
+bool buttonPressed(uint8_t pinMask, uint8_t index)
 {
-  bool state = digitalRead(pin);
+  bool state = (PIND & pinMask) ? true : false;
 
   if (state && !(lastBtnState & (1 << index)))
   {
@@ -51,24 +55,26 @@ void motorUpdate()
 {
   if (!motorEnable)
   {
-    digitalWrite(L298_IN1, LOW);
-    digitalWrite(L298_IN2, LOW);
-    analogWrite(L298_ENA, 0);
+    // off: IN1 = LOW, IN2 = LOW
+    PORTC &= ~((1 << IN1_PIN) | (1 << IN2_PIN));
+    OCR1A = 0;
     return;
   }
 
   if (motorDirection)
   {
-    digitalWrite(L298_IN1, HIGH);
-    digitalWrite(L298_IN2, LOW);
+    // forward: IN1 = HIGH, IN2 = LOW
+    PORTC |= (1 << IN1_PIN);
+    PORTC &= ~(1 << IN2_PIN);
   }
   else
   {
-    digitalWrite(L298_IN1, LOW);
-    digitalWrite(L298_IN2, HIGH);
+    // reverse: IN1 = LOW, IN2 = HIGH
+    PORTC &= ~(1 << IN1_PIN);
+    PORTC |= (1 << IN2_PIN);
   }
 
-  analogWrite(L298_ENA, pwmValue);
+  OCR1A = pwmValue;
 }
 
 void oledUpdate(uint8_t mode)
@@ -90,34 +96,52 @@ void setup()
   oled.setPowerSave(0);
   oled.setFont(u8x8_font_8x13_1x2_r);
 
-  pinMode(POTENTIONMETER, INPUT);
-  pinMode(SW2, INPUT);
-  pinMode(SW3, INPUT);
-  pinMode(SW4, INPUT);
-  pinMode(SW5, INPUT);
+  // ADC
+  ADMUX = (1 << REFS0);
+  ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // 128
 
-  pinMode(L298_ENA, OUTPUT);
-  pinMode(L298_IN1, OUTPUT);
-  pinMode(L298_IN2, OUTPUT);
+  // IO
+  DDRD &= ~((1 << SW2_PIN) | (1 << SW3_PIN) | (1 << SW4_PIN) | (1 << SW5_PIN));
+  PORTD &= ~((1 << SW2_PIN) | (1 << SW3_PIN) | (1 << SW4_PIN) | (1 << SW5_PIN));
 
-  analogWrite(L298_ENA, 0);
+  DDRC |= (1 << IN1_PIN) | (1 << IN2_PIN);
+  DDRB |= (1 << ENA_PIN);
+
+  DDRC &= ~(1 << POTENTIONMETER_PIN);
+
+  // Timer1
+  TCCR1A = (1 << COM1A1) | (1 << WGM10);
+  TCCR1B = (1 << WGM12) | (1 << CS10);
+  OCR1A = 0;
+
+  // init
+  PORTC &= ~((1 << IN1_PIN) | (1 << IN2_PIN));
 }
 
 void loop()
 {
-  uint16_t adcValue = analogRead(POTENTIONMETER);
+  ADCSRA |= (1 << ADSC);
+  while (ADCSRA & (1 << ADSC))
+    ;
+  uint16_t adcValue = ADC;
+
   uint8_t Mode_Control = adcValue / 113;
   if (Mode_Control > 8)
     Mode_Control = 8;
 
   pwmValue = map(Mode_Control, 0, 8, 0, 255);
 
-  if (buttonPressed(SW2, 0))    motorEnable = true;
-  if (buttonPressed(SW3, 1))    motorEnable = false;
-  if (buttonPressed(SW4, 2))    motorDirection = true;
-  if (buttonPressed(SW5, 3))    motorDirection = false;
+  if (buttonPressed((1 << SW2_PIN), 0))
+    motorEnable = true;
+  if (buttonPressed((1 << SW3_PIN), 1))
+    motorEnable = false;
+  if (buttonPressed((1 << SW4_PIN), 2))
+    motorDirection = true;
+  if (buttonPressed((1 << SW5_PIN), 3))
+    motorDirection = false;
 
   motorUpdate();
+
   if (Mode_Control != prevMode ||
       motorEnable != prevMotorEnable ||
       motorDirection != prevMotorDirection)
